972997ea4b6bfa275c04d85dadda4c3ab76ec2f8
wangyu73 <wangyu73@58.com>
2020-06-27 10:41:12 +0800

    fixed

---
+++ bundler.js
@@ -0,0 +1,100 @@
+const fs = require('fs');
+const path = require('path');
+const babyloan = require('@babel/parser');
+const traverse = require('@babel/traverse').default;
+const babel = require('@babel/core');
+
+let ID = 0;
+
+function createAsset(filename) {
+    //获取文件返回字符串
+    const content = fs.readFileSync(filename, 'utf-8');
+
+    //babyloan 解析字符串为ast
+    const ast = babyloan.parse(content, {
+        sourceType: 'module'
+    })
+
+    const dependencies = [];
+
+    traverse(ast, {
+        ImportDeclaration: ({node}) => {
+            dependencies.push(node.source.value);
+        }
+    })
+
+    const id = ID++;
+
+    const { code } = babel.transformFromAstSync(ast, null, {
+        presets: ['@babel/preset-env']
+    });
+
+    return {
+        id,
+        filename,
+        dependencies,
+        code
+    }
+}
+
+function createGraph(entry)  {
+    const mainAsset = createAsset(entry);
+
+    const queue = [mainAsset];
+    console.log('=====queue=====', queue)
+    for (const asset of queue) {
+        // console.log('=====queue asset=====', queue)
+        const dirname = path.dirname(asset.filename);
+        console.log(asset);
+        asset.mapping = {};
+
+        asset.dependencies.forEach(relativePath => {
+            const absolutePath = path.join(dirname, relativePath);
+
+            const child = createAsset(absolutePath);
+            // console.log(child)
+            asset.mapping[relativePath] = child.id;
+
+            queue.push(child)
+        })
+    }
+
+    return queue;
+}
+
+function bundle(graph) {
+    let modules = "";
+    // console.log('bundle')
+    graph.forEach(mod => {
+        modules += `${mod.id}: [
+            function (require, module, exports) {
+                ${mod.code}
+            },
+            ${JSON.stringify(mod.mapping)}
+        ],`
+    });
+    // console.log(graph)
+    const result = `
+        (function (modules) {
+            function require(id) {
+                const [fn, mapping] = modules[id];
+                function localRequire(relativePath) {
+                    return require(mapping[relativePath])
+                }
+                const module = {exports: {}};
+
+                fn(localRequire, module, module.exports);
+                return module.exports
+            }
+
+            require(0)
+        })({${modules}})
+    `
+
+    return result
+}
+
+const graph = createGraph('./example/entry.js');
+const ret = bundle(graph);
+
+fs.writeFileSync('./bundle.js', ret)
---
+++ example/name.js
@@ -0,0 +1,3 @@
+import hello from './hello.js'
+
+export const name = `${hello} world`;
